1cefbf935ec414f173418637d6bcbb8601cd883d
parkhorim <horimpark@dramancompany.com>
2024-06-02 11:00:34 +0900

    yes

---
+++ codewars/5kyu/Vector class.py
@@ -0,0 +1,33 @@
+import math
+
+
+class Vector:
+    def __init__(self, components):
+        self.components = components
+
+    def __str__(self):
+        return f"({','.join(map(str, self.components))})"
+
+    def __eq__(self, other):
+        return self.components == other.components
+
+    def equals(self, other):
+        return self.__eq__(other)
+
+    def add(self, other):
+        if len(self.components) != len(other.components):
+            raise ValueError("not same length")
+        return Vector([x + y for x, y in zip(self.components, other.components)])
+
+    def subtract(self, other):
+        if len(self.components) != len(other.components):
+            raise ValueError("not same length")
+        return Vector([x - y for x, y in zip(self.components, other.components)])
+
+    def dot(self, other):
+        if len(self.components) != len(other.components):
+            raise ValueError("not same length")
+        return sum(x * y for x, y in zip(self.components, other.components))
+
+    def norm(self):
+        return math.sqrt(sum(x**2 for x in self.components))
---
+++ codewars/6kyu/Stop gninnipS My sdroW!.py
@@ -0,0 +1,8 @@
+def spin_words(sentence):
+    result = []
+    for x in sentence.split(' '):
+        if len(x) > 4:
+            result.append(x[::-1])
+        else:
+            result.append(x)
+    return " ".join(result)
