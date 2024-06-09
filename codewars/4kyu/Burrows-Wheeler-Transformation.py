c63580570312f2b03fe7971413157dd41dad7e0a
parkhorim <horimpark@dramancompany.com>
2024-06-09 16:57:37 +0900

    yes

---
+++ codewars/4kyu/Burrows-Wheeler-Transformation.py
@@ -0,0 +1,31 @@
+import codewars_test as test
+
+
+# 명시적으로 tuple로 반환해야 함, test 코드에 명시적으로 ecode(s)[0] 식으로 호출하기 때문
+def encode(s):
+    if s == "":
+        return ("", 0)
+    else:
+        sorted_total = sorted([s[i + 1 :] + s[:i] + s[i] for i in range(len(s))])
+        print(f"string : {s}")
+        print(sorted_total)
+        return ("".join([x[-1] for x in sorted_total]), sorted_total.index(s))
+
+
+def decode(s, n):
+    if s == "":
+        return ""
+    sorted_total = [""] * len(s)
+    for _ in range(len(s)):
+        sorted_total = sorted([s[i] + sorted_total[i] for i in range(len(s))])
+    print(f"string : {s}, index : {n}")
+    print(sorted_total)
+    return sorted_total[n]
+
+
+test.assert_equals(encode("bananabar"), ("nnbbraaaa", 4))
+test.assert_equals(encode("Humble Bundle"), ("e emnllbduuHB", 2))
+test.assert_equals(encode("Mellow Yellow"), ("ww MYeelllloo", 1))
+test.assert_equals(decode("nnbbraaaa", 4), "bananabar")
+test.assert_equals(decode("e emnllbduuHB", 2), "Humble Bundle")
+test.assert_equals(decode("ww MYeelllloo", 1), "Mellow Yellow")
---
+++ codewars/4kyu/Next bigger number with the same digits.py
@@ -0,0 +1,44 @@
+# from itertools import permutations
+#
+#
+# def gen_permutations(num_list):
+#     numbers = set()
+#     for i in range(1, len(num_list) + 1):
+#         for p in permutations(num_list, i):
+#             n = int("".join(map(str, p)))
+#             numbers.add(n)
+#     return sorted(numbers)
+#
+#
+# def next_bigger(n):
+#     split_n = [int(x) for x in str(n)]
+#     sorted_nums = gen_permutations(split_n)
+#     if sorted_nums[-1] == n:
+#         return -1
+#     else:
+#         idx = sorted_nums.index(n) + 1
+#         return sorted_nums[idx]
+#
+#     return n
+
+
+# 시간 초과..
+
+
+def next_bigger(n):
+    split_n = list(str(n))
+    length = len(split_n)
+
+    for i in range(length - 2, -1, -1):
+        if split_n[i] < split_n[i + 1]:
+            break
+    else:
+        return -1
+
+    for j in range(length - 1, i, -1):
+        if split_n[j] > split_n[i]:
+            break
+
+    split_n[i], split_n[j] = split_n[j], split_n[i]
+    split_n = split_n[: i + 1] + sorted(split_n[i + 1 :])
+    return int("".join(split_n))
