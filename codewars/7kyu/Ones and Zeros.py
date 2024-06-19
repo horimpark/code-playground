7e9c1661dd1072edc856c25b685b695ed98641b0
parkhorim <horimpark@dramancompany.com>
2024-06-19 14:54:13 +0900

    6/19

---
+++ codewars/7kyu/Ones and Zeros.py
@@ -0,0 +1,19 @@
+def binary_array_to_number(arr):
+    arr.reverse()
+    sum = 0
+    for i, x in enumerate(arr):
+        sum += (2**i) * x
+    return sum
+
+
+import codewars_test as test
+
+
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(binary_array_to_number([0,0,0,1]), 1)
+        test.assert_equals(binary_array_to_number([0,0,1,0]), 2)
+        test.assert_equals(binary_array_to_number([1,1,1,1]), 15)
+        test.assert_equals(binary_array_to_number([0,1,1,0]), 6)
