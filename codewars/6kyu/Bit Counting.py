5f1644ef64df1c8a536e97954a265b6baf8e34f0
parkhorim <horimpark@dramancompany.com>
2024-08-18 13:51:29 +0900

    horim 8/17

--- codewars/6kyu/Bit Counting.py
+++ codewars/6kyu/Bit Counting.py
@@ -1,5 +1,11 @@
 def count_bits(n):
     return bin(n).count("1")
 
+
 print(count_bits(1234))  # 5
 print(count_bits(4))  # 1
+
+
+# horim style
+# def count_bits(n):
+#     return sum([int(x) for x in str(bin(n)[2:])])
