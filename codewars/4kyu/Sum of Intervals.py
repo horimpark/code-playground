4b0abe4b6707ec4f8dd26a3f7455d83f92e6f316
parkhorim <horimpark@dramancompany.com>
2024-11-13 08:03:13 +0900

    horim 11/13

--- codewars/4kyu/Sum of Intervals.py
+++ codewars/4kyu/Sum of Intervals.py
@@ -1,13 +1,15 @@
 def sum_of_intervals(intervals):
-    intervals = sorted(intervals, key=lambda x: x[0])
-    check = []
-    for x in intervals:
-        if not check:
-            check.append(x)
-        else:
-            for i, c in enumerate(check):
-                if list(c)[-1] >= x[0]:
-                    check[i] = sorted(list(set(c + x)))
-                else:
-                    check.append(x)
-    return sum([x[-1] - x[0] for x in check])
+    ordered_intervals = [(i, 1) for i, _ in intervals] + [(j, -1) for _, j in intervals]
+    ordered_intervals.sort(key=lambda x: x[0])
+    total_len = 0
+    start = ordered_intervals[0]
+    open_interval_count = 1
+
+    for i, k in ordered_intervals[1:]:
+        if start == None:
+            start = (i, k)
+        open_interval_count += k
+        if open_interval_count == 0:
+            total_len += i - start[0]
+            start = None
+    return total_len
