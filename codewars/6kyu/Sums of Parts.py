f0c8149064bac3331ebd0b87a51f070949cadaa6
parkhorim <horimpark@dramancompany.com>
2024-11-19 10:05:39 +0900

    me too

--- codewars/6kyu/Sums of Parts.py
+++ codewars/6kyu/Sums of Parts.py
@@ -2,6 +2,7 @@
 def parts_sums(ls):
     return [sum(ls[i:]) for i in range(len(ls))] + [0]
 
+
 # 성공
 def parts_sums(ls):
     total_sum = sum(ls)
@@ -16,3 +17,11 @@ if __name__ == "__main__":
     print(parts_sums([0, 1, 3, 6, 10]))  # [20, 20, 19, 16, 10, 0]
     print(parts_sums([1, 2, 3, 4, 5, 6]))  # [21, 20, 18, 15, 11, 6, 0]
     print(parts_sums([]))  # [0]
+
+
+def parts_sums(ls):
+    result = [sum(ls)]
+
+    for x in ls:
+        result.append(result[-1] - x)
+    return result
