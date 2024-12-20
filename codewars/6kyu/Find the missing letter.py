88f0d01e2edb4cc57fb5e77d02d88468e5cca987
hh.hwang <hhhwang94@naver.com>
2024-12-20 21:24:19 +0900

    feat : 12/20 문제

--- codewars/6kyu/Find the missing letter.py
+++ codewars/6kyu/Find the missing letter.py
@@ -5,3 +5,9 @@ def find_missing_letter(chars):
     for x, y in zip(range(all_c.index(chars[0]), all_c.index(chars[-1])), chars):
         if all_c[x] != y:
             return all_c[x]
+
+
+def find_missing_letter(chars):
+    for i in range(len(chars) - 1):
+        if ord(chars[i + 1]) - ord(chars[i]) > 1:
+            return chr(ord(chars[i]) + 1)
