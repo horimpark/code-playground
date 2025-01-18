6c1bba35da7434c409329290542489c173344a0e
hh.hwang <hhhwang94@naver.com>
2025-01-18 20:27:19 +0900

    feat : 1/18 문제

--- codewars/7kyu/The Coupon Code.py
+++ codewars/7kyu/The Coupon Code.py
@@ -1,6 +1,9 @@
+from datetime import datetime
+
+
 def check_coupon(entered_code, correct_code, current_date, expiration_date):
     first_cond = entered_code == correct_code
-    second_cond = current_date == expiration_date
+    second_cond = datetime.strptime(current_date, "%B %d, %Y") >= datetime.strptime(expiration_date, "%B %d, %Y")
     if first_cond | second_cond:
         return True
     else:
