ab2c33c7b59b01463dd783861d73d208b343026b
hh.hwang <hh.hwang@dramancompany.com>
2025-02-12 19:11:41 +0900

    feat : 2/12 문제

---
+++ codewars/6kyu/A More Complicated Matrix Expected Value.py
@@ -0,0 +1,11 @@
+from fractions import Fraction
+
+
+def expected_value(c: int, r: int) -> Fraction:
+    total = 0
+    for start_num in range(r + 1):
+        total += sum([n ** 2 for n in range(start_num, start_num + c + 1)])
+
+    low = (c + 1) * (r + 1)
+
+    return Fraction(total, low)
