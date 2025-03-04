f67cf4bbe160caee25099d1ddf7aaa736b8d67f9
HohyunH <hh.hwang@dramancompany.com>
2025-03-04 21:03:16 +0900

    feat : 3/4 문제

--- codewars/6kyu/Which are in?.py
+++ codewars/6kyu/Which are in?.py
@@ -6,3 +6,11 @@ def in_array(array1, array2):
                 res.append(x)
                 break
     return sorted(set(res))
+
+
+def in_array(array1, array2):
+    answer = []
+    for a1 in array1:
+        if any(a1 in a2 for a2 in array2) and a1 not in answer:
+            answer.append(a1)
+    return sorted(answer)
