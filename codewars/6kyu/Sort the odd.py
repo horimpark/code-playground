fea5f358d1fe4d5a22cee114187793b729d76b17
HohyunH <hh.hwang@dramancompany.com>
2025-03-06 21:31:16 +0900

    feat : 3/6 문제

--- codewars/6kyu/Sort the odd.py
+++ codewars/6kyu/Sort the odd.py
@@ -19,3 +19,18 @@ def sort_array(source_array):
 # def sort_array(arr):
 #   odds = sorted((x for x in arr if x%2 != 0), reverse=True)
 #   return [x if x%2==0 else odds.pop() for x in arr]
+
+def sort_array(source_array):
+    if not source_array:
+        return []
+    index_ = [i for i, s in enumerate(source_array) if s % 2]
+    nums_ = [s for i, s in enumerate(source_array) if s % 2]
+    nums_ = sorted(nums_)
+
+    idx_to_num = {i: n for i, n in zip(index_, nums_)}
+
+    answer = []
+    for i, s in enumerate(source_array):
+        answer.append(idx_to_num.get(i, s))
+
+    return answer
