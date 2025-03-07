c5cb97c22797be1c78e94cc91b81544519ccfb89
HohyunH <hh.hwang@dramancompany.com>
2025-03-07 17:29:43 +0900

    feat : 3/7 문제

--- codewars/5kyu/Human Readable Time.py
+++ codewars/5kyu/Human Readable Time.py
@@ -6,3 +6,9 @@ print(make_readable(3599))
 print(make_readable(86399))
 print(make_readable(59))
 print(make_readable(60))
+
+
+def make_readable(seconds):
+    hours, seconds = divmod(seconds, 60 ** 2)
+    minutes, seconds = divmod(seconds, 60)
+    return '{:02}:{:02}:{:02}'.format(hours, minutes, seconds)
