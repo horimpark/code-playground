1e312c1f4d796c0d5ed23f2ef05f13bcb6054436
HohyunH <hh.hwang@dramancompany.com>
2025-03-10 21:58:00 +0900

    feat : 3/10 문제

--- codewars/6kyu/Consecutive strings.py
+++ codewars/6kyu/Consecutive strings.py
@@ -7,3 +7,17 @@ def longest_consec(strarr, k):
     length = [len(x) for x in res]
     max_idx = length.index(max(length))
     return res[max_idx]
+
+
+def longest_consec(strarr, k):
+    if not strarr or k <= 0 or k > len(strarr):
+        return ""
+
+    candidates = []
+    for i in range(len(strarr) - k + 1):
+        combined_str = "".join(strarr[i:i + k])
+        candidates.append((combined_str, len(combined_str)))
+
+    sorted_candidates = sorted(candidates, key=lambda x: (-x[1], x[0]))
+
+    return sorted_candidates[0][0] if sorted_candidates else ""
