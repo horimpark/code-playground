3f4fd86508301b7c49f7bb9b23b894b10b0e481e
HohyunH <hh.hwang@dramancompany.com>
2025-03-18 22:00:56 +0900

    feat : 3/18 문제

--- codewars/7kyu/Replace all items.py
+++ codewars/7kyu/Replace all items.py
@@ -1,10 +1,4 @@
-def replace_str(s, f, r):
-    if type(s) == int:
-        return int(str(s).replace(str(f), str(r)))
-    else:
-        return s.replace(f, r)
-
 def replace_all(obj, find, replace):
     if type(obj) == str:
         return obj.replace(find, replace)
-    return [replace_str(o, find, replace) for o in obj]
+    return [replace if o == find else o for o in obj]
