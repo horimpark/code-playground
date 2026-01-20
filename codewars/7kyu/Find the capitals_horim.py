812fba03a1c6421f48b37aa0f7834a4f70c072bb
horimpark <ghfla543@gmail.com>
2026-01-20 11:02:53 +0900

    horim 260120

---
+++ codewars/7kyu/Find the capitals_horim.py
@@ -0,0 +1,3 @@
+def capitals(word):
+    index = [i for i in range(len(word)) if word[i].isupper()]
+    return index
---
+++ codewars/8kyu/Drink about.py
@@ -0,0 +1,11 @@
+def people_with_age_drink(age):
+    drink = ""
+    if age < 14:
+        drink = "toddy"
+    elif age < 18:
+        drink = "coke"
+    elif age < 21:
+        drink = "beer"
+    else:
+        drink = "whisky"
+    return f"drink {drink}"
